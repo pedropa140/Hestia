8cd6dd2392ca07129d2ae1e3d46c34a25798d5f9
Elian <elian.dap@outlook.com>
2024-04-29 15:20:57 -0400

    add TickerData placeholder

--- Hestia/application/app/models.py
+++ Hestia/application/app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-
-		
+class TickerData(models.Model):
+    # Define your TickerData model fields here
+    ticker_symbol = models.CharField(max_length=10)
+    company_name = models.CharField(max_length=100)
+    # Add more fields as needed

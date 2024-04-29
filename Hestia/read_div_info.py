608d4cbd212080c8d971575cdbe3c2e1a9773465
Pedro A. Pajarillo Jr <pedropa140@gmail.com>
2024-04-29 15:23:29 -0400

    added html

---
+++ Hestia/read_div_info.py
@@ -0,0 +1,29 @@
+import os
+
+def check_folder(directory):
+    folder_path = os.path.join(directory)
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        return True
+    else:
+        return False
+
+def read_div():
+    directory = './stockdata/div_info/'
+    
+    info_list = []
+    if check_folder(directory):
+
+        div_info_files = os.listdir(directory)
+        info = []
+        for filename in div_info_files:
+            with open(directory + filename, 'r') as file:
+            # with open(directory + 'WSFS.csv', 'r') as file:
+                ignore_line = file.readline()
+                lines = file.readlines()
+                for line in lines:
+                    line = line.replace('\n', '').replace('\"', '').split(',')
+                    info_list.append(line[-15:])
+    return info_list
+
+if __name__ == '__main__':
+    print(read_div())

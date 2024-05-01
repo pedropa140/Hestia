3706e38bea499ac55295508e1b51f8ae50f08e29
Elian <elian.dap@outlook.com>
2024-05-01 03:15:03 -0400

    fix proxy

--- Hestia/application/project/settings.py
+++ Hestia/application/project/settings.py
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
